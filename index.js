b0317953996172eb2ac60f014a40028501c559f0
Hannah Malm <hannahemalm616@gmail.com>
2021-09-10 19:03:41 -0400

    commit tree 0014da10d3bcd02d8b80ef53c6a5b3870b944a5b

--- index.js
+++ index.js
@@ -25,24 +25,82 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const [bessie, ,dolly, babe, little] = farmAnimals.split(" ");
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+// you need to leave a blank space for anything missing
+const [blackAndWhite, , black, pink] = farmAnimals.split(" ");
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors; 
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+//add a blank space for indigo
+const [r,o,y,g,b, , v] = colors;
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// add blank spaces for the other colors
+const [, , , , , indg] = colors;
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const {muppetName, color, song, job, partner} = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {nestedName, nestedColor, album, nestedJob, nestedPartner} = nestedMuppet;
+const { song2, song4} = nestedMuppet.album.theMuppetMovie;
+
+
+
+
+
+
+
+// Code Process
+
+//Declare a variable with a name (e.g. name or breed)
+//Use that variable's name to point to an attribute in the Object whose name matches the name of the variable (e.g. doggie.breed or doggie.name)
+//Assign the attribute's value to the created variable
+
+
+
+
+
+// const doggie = {
+//   name: 'Buzz',
+//   breed: 'Great Pyrenees',
+//   furColor: 'black and white',
+//   activityLevel: 'sloth-like',
+//   favoriteFood: 'hot dogs'
+// };
+
+// const { name, breed } = doggie;
+
+
+
+// const doggie = {
+//   name: 'Buzz',
+//   breed: 'Great Pyrenees',
+//   furColor: 'black and white',
+//   activityLevel: 'sloth-like',
+//   favoriteFoods: {
+//     meats:{
+//       ham: 'smoked',
+//       hotDog: 'Oscar Meyer',
+//     },
+//     cheeses:{
+//       american: 'kraft'
+//     }
+//   }
+// };
+
+// const { ham, hotDog } = doggie.favoriteFoods.meats;
